225bdafdc4058c627e0d3d431c4455c9f595ce2c
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-10-01 20:36:53 +0900

    add fastapi sample

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from fastmcp import FastMCP
-
-mcp = FastMCP("My MCP Server")
-
-
-@mcp.tool
-def greet(name: str) -> str:
-    return f"Hello, {name}!"
+from pydantic import BaseModel
+from fastapi import FastAPI
 
 
-if __name__ == "__main__":
-    mcp.run(transport="http", port=8000)
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+
+app = FastAPI()
+
+users: dict[int, User] = {}
+
+
+@app.post("/users")
+def create_user(user: User):
+    users[user.id] = user
+    return users[user.id]
+
+
+@app.get("/users")
+def read_users():
+    return list(users.values())
+
+
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    user = users[user_id]
+    return {"id": user_id, "name": user.name, "age": user.age}
