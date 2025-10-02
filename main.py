14658b9e76db9341a0b05b33e90e66a7b7da4fe1
Tatsu015 <tatsumi.soichiro@gmail.com>
2025-10-02 19:57:40 +0900

    add mcp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastmcp import FastMCP
 
 
 class User(BaseModel):
@@ -8,23 +9,35 @@ class User(BaseModel):
     age: int
 
 
-app = FastAPI()
+api_app = FastAPI()
 
 users: dict[int, User] = {}
 
 
-@app.post("/users")
+@api_app.post("/users")
 def create_user(user: User):
     users[user.id] = user
     return users[user.id]
 
 
-@app.get("/users")
+@api_app.get("/users")
 def read_users():
     return list(users.values())
 
 
-@app.get("/users/{user_id}")
+@api_app.get("/users/{user_id}")
 def read_user(user_id: int):
     user = users[user_id]
     return {"id": user_id, "name": user.name, "age": user.age}
+
+
+mcp = FastMCP.from_fastapi(app=api_app, name="E-commerce MCP")
+mcp_app = mcp.http_app(path="/mcp")
+app = FastAPI(
+    title="E-commerce API with MCP",
+    routes=[
+        *mcp_app.routes,  # MCP routes
+        *api_app.routes,  # Original API routes
+    ],
+    lifespan=mcp_app.lifespan,
+)
